68deb290c376abca73dce5deed0963b09305dfac
Tushar Verma <tuhsar2022@gmail.com>
2024-02-18 13:32:51 +0530

    added user model

---
+++ backend/src/models/users.js
@@ -0,0 +1,72 @@
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      required: false,
+    },
+    username: {
+      type: String,
+      min: 3,
+      max: 15,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      //   minlength:8
+    },
+    bio: {
+      type: String,
+      trim: true,
+    },
+    profilePhoto: {
+      type: String,
+      default: "",
+    },
+    coverPhoto: {
+      type: String,
+      default: "",
+    },
+    posts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    blockList: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const User = model("User", userSchema);
+
+export default User;
