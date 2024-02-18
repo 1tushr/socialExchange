31a5b0cedaa8ae87d05ff242a493f07901759f80
Tushar Verma <tuhsar2022@gmail.com>
2024-02-18 14:24:44 +0530

    added post schema

---
+++ backend/src/models/post.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const postSchema = new Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    caption: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Post = model("Post", postSchema);
