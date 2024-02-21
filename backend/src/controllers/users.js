c657bd7e77403963c027c9d3eb0cedec55cdf35d
Tushar Verma <tuhsar2022@gmail.com>
2024-02-21 19:32:33 +0530

    added follow user router and controller

--- backend/src/controllers/users.js
+++ backend/src/controllers/users.js
@@ -3,7 +3,7 @@ import * as HttpStatusCodes from "../constants/httpStatusCode.js";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 dotenv.config();
-const { HTTP_OK, HTTP_BAD_REQUEST, HTTP_INTERNAL_SERVER_ERROR } =
+const { HTTP_OK, HTTP_BAD_REQUEST, HTTP_INTERNAL_SERVER_ERROR, HTTP_CREATED } =
   HttpStatusCodes;
 
 //function to get user
@@ -31,6 +31,7 @@ async function handleGetUser(req, res) {
   }
 }
 
+// function for update user
 async function handleUpdateUser(req, res) {
   try {
     const { userId } = req.params;
@@ -62,4 +63,57 @@ async function handleUpdateUser(req, res) {
   }
 }
 
-export { handleGetUser, handleUpdateUser };
+// function for follow user
+async function handleFollowUser(req, res) {
+  try {
+    const { userId } = req.params;
+    const userToFollowId = req.body;
+
+    // Check if the provided user ID is the same as the user to follow ID
+    if (userId.toString() === userToFollowId._id?.toString()) {
+      return res
+        .status(HTTP_BAD_REQUEST)
+        .json({ message: "Cannot follow yourself" });
+    }
+
+    // Fetch the user initiating the follow
+    const user = await User.findById(userId);
+
+    // Fetch the user to follow
+    const userToFollow = await User.findById(userToFollowId);
+
+    // Check if either user is not found
+    if (!user || !userToFollow) {
+      return res
+        .status(HTTP_BAD_REQUEST)
+        .json({ message: "Invalid user ID provided for follow operation" });
+    }
+
+    //Check if user is already followed
+    if (
+      user.following.includes(userToFollowId._id) ||
+      userToFollow.followers.includes(userId)
+    ) {
+      return res
+        .status(HTTP_BAD_REQUEST)
+        .json({ message: "You are already following this user" });
+    }
+
+    // Update the following and followers arrays
+    user.following.push(userToFollow);
+    userToFollow.followers.push(user._id);
+
+    // Save changes to the database
+    await user.save();
+    await userToFollow.save();
+    res.status(HTTP_OK).json({ message: "User followed successfully" });
+  } catch (error) {
+    console.error(error);
+    return res.status(HTTP_INTERNAL_SERVER_ERROR).json({
+      message: "Error occurred during follow operation",
+      details: error.message,
+    });
+  }
+}
+
+export { handleGetUser, handleUpdateUser, handleFollowUser };
--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,9 +1,24 @@
 import express from "express";
-import { handleGetUser, handleUpdateUser } from "../controllers/users.js";
+import {
+  handleGetUser,
+  handleUpdateUser,
+  handleFollowUser,
+} from "../controllers/users.js";
 import isVerified from "../middlewares/isVerified.js";
 
 const userRouter = express.Router();
 
+//getuser
 userRouter.get("/getuser/:userId", isVerified, handleGetUser);
+
+//update userdata
 userRouter.post("/updateuser/:userId", isVerified, handleUpdateUser);
+
+// follow user
+userRouter.post("/followuser/:userId", isVerified, handleFollowUser);
+
+//unfollow user
+//block user
+//unblock user
+//delete user
 export default userRouter;
