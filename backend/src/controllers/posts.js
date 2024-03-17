2ab5ff1357838d58d771d39ea2fc0ae99683cd3b
Tushar Verma <tuhsar2022@gmail.com>
2024-03-18 00:17:01 +0530

    added update post route

--- backend/src/controllers/posts.js
+++ backend/src/controllers/posts.js
@@ -1,13 +1,17 @@
-import Post from "../models/post.js";
 import User from "../models/users.js";
-import { HTTP_BAD_REQUEST, HTTP_CREATED } from "../constants/httpStatusCode.js";
+import Post from "../models/post.js";
+import {
+  HTTP_BAD_REQUEST,
+  HTTP_CREATED,
+  HTTP_OK,
+} from "../constants/httpStatusCode.js";
 import mongoose from "mongoose";
 
 async function createNewpost(req, res) {
   try {
     // Extract user ID from request parameters and caption from request body
     const { userid } = req.params;
-    const { caption } = req.body;
+    const { caption, image } = req.body;
 
     // Validate that userid is a valid MongoDB ObjectId
     if (!mongoose.Types.ObjectId.isValid(userid)) {
@@ -35,21 +39,63 @@ async function createNewpost(req, res) {
         .json({ message: "Caption is required" });
     }
 
+    const newPostData = { user, caption };
+    if (image) {
+      newPostData.image = image;
+    }
     // If all conditions are met, create a new post with user and caption
-    const newPost = await Post.create({
-      user,
-      caption,
-    });
+
+    const newPost = await Post.create(newPostData);
 
     // Save the new post to the database
     await newPost.save();
 
     // Respond with a success status and a message
-    res.status(HTTP_CREATED).json({ message: "New post created successfully" });
+    res
+      .status(HTTP_CREATED)
+      .json({ message: "New post created successfully", newPost });
   } catch (error) {
     // If an error occurs during the process, handle it and respond with an error status
     console.error("Error creating post:", error);
     res.status(HTTP_BAD_REQUEST).json({ message: "Error creating post" });
   }
 }
-export default createNewpost;
+
+async function updatePost(req, res) {
+  try {
+    const { postid } = req.params;
+    const { caption } = req.body;
+    console.log(req.params);
+    // Check if postId is a valid ObjectId
+    if (!mongoose.Types.ObjectId.isValid(postid)) {
+      return res
+        .status(HTTP_BAD_REQUEST)
+        .json({ message: "Invalid post ID format" });
+    }
+
+    // Update the post and return the updated document
+    const updatedPost = await Post.findOneAndUpdate(
+      { _id: postid },
+      { caption: caption },
+      { new: true } // Return the updated document
+    );
+
+    // Check if a post was found and updated
+    if (!updatedPost) {
+      // If no post was found with the provided postid, return a 400 Bad Request response
+      return res.status(HTTP_BAD_REQUEST).json({ message: "No post found" });
+    }
+
+    // Return a 200 OK response with the updated post
+    res
+      .status(HTTP_OK)
+      .json({ message: "Post updated successfully", post: updatedPost });
+  } catch (error) {
+    // Log any errors that occur during the update process
+    console.error("Error updating post:", error);
+    // Return a 400 Bad Request response with an error message
+    res.status(HTTP_BAD_REQUEST).json({ message: "Error in updating post" });
+  }
+}
+
+export { createNewpost, updatePost };
--- backend/src/routes/posts.js
+++ backend/src/routes/posts.js
@@ -1,13 +1,13 @@
 import isVerified from "../middlewares/isVerified.js";
 import express from "express";
-import createNewpost from "../controllers/posts.js";
+import { createNewpost, updatePost } from "../controllers/posts.js";
 const postRouter = express.Router();
 
 //create post
 postRouter.post("/createpost/:userid", isVerified, createNewpost);
 
 //update post
-
+postRouter.put("/updatepost/:postid", updatePost);
 //get all post
 
 //delete post
